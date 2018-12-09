82cebb7b7e721e5e6e34bc4d99b36d438b4dd38d
Artur Fogiel <arturprojekty1988@gmail.com>
2018-12-09 20:57:04 +0100

    Initial commit

---
+++ VTHelper/VTHelper/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using VirusTotalNET;
+using VirusTotalNET.Results;
+
+namespace VTHelper
+{
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
+    public partial class App : Application
+    {        
+        public static string GetAPIKey()
+        {
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText("API.txt"))
+            {
+                string s = sr.ReadLine();
+                return s;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public static async System.Threading.Tasks.Task ScanDomainAsync(string domain)
+        {
+            VirusTotal vt = new VirusTotal(App.GetAPIKey());
+            DomainReport domainReport = await vt.GetDomainReportAsync(domain);
+        }
+    }
+}
---
+++ VTHelper/VTHelper/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+namespace VTHelper
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            Nullable<bool> result = openFileDialog1.ShowDialog();
+            if (result == true)
+            {
+                FileInfo fileInfo = new FileInfo(openFileDialog1.FileName);
+                string sha256 = VirusTotalNET.Helpers.HashHelper.GetSHA256(fileInfo);
+
+                App.ScanDomainAsync("google.pl");
+            }
+        }
+    }
+}
