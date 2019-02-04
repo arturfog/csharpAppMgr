d4ac5d0f01d0f4521085000306da4b4c973533c1
Artur Fogiel <arturprojekty1988@gmail.com>
2019-02-04 19:56:00 +0100

    Connected first UI controls with DomainReport results

--- VTHelper/VTHelper/MainWindow.xaml.cs
+++ VTHelper/VTHelper/MainWindow.xaml.cs
@@ -15,27 +15,47 @@ namespace VTHelper
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="domain"></param>
         private async void ParseDomainReportAsync(string domain)
         {
             DomainReport domainReport = await App.ScanDomainAsync(domain);
+            BitDefenderDomainCat_Lbl.Content = domainReport.BitDefenderCategory;
+            ForcePointDomainCat_Lbl.Content = domainReport.ForcePointThreatSeekerCategory;
+            WHOIS_Lbl.Text = domainReport.WhoIs;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath"></param>
         private async void ParseFileReportAsync(string filePath)
         {
             FileReport fileReport = await App.ScanFileAsync(filePath);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ip"></param>
         private async void ParseIPReportAsync(string ip)
         {
             IPReport iPReport = await App.IPReportAsync(ip);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GetDomainReportBtn_Click(object sender, RoutedEventArgs e)
         {
             ParseDomainReportAsync(DomainName_TextBox.Text);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GetFileReportBtn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
@@ -48,17 +68,30 @@ namespace VTHelper
                 FileName_TextBox.Text = fileInfo.FullName;
             }
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ScanFileTabBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ScanFile_Panel.Visibility = Visibility.Visible;
+            ScanDomain_Panel.Visibility = Visibility.Hidden;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ScanDomainTabBtn_Click(object sender, RoutedEventArgs e)
         {
             ScanFile_Panel.Visibility = Visibility.Hidden;
             ScanDomain_Panel.Visibility = Visibility.Visible;
         }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ScanFile_Panel.Visibility = Visibility.Visible;
-            ScanDomain_Panel.Visibility = Visibility.Hidden;
+            ParseFileReportAsync(FileName_TextBox.Text);
         }
     }
 }
